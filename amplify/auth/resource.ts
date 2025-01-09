aeb94fb6154cc57d1a88f7ff1b7d1cd2d41558d0
bbzam <33444249+bbzam@users.noreply.github.com>
2025-01-09 15:11:10 +0800

    Initial commit

---
+++ amplify/auth/resource.ts
@@ -0,0 +1,25 @@
+import { defineAuth } from '@aws-amplify/backend';
+
+/**
+ * Define and configure your auth resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+  userAttributes: {
+    givenName: {
+      mutable: true,
+      required: false,
+    },
+    familyName: {
+      mutable: true,
+      required: false,
+    },
+    birthdate: {
+      mutable: true,
+      required: false,
+    }
+  },
+});
