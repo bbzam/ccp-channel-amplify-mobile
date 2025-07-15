f4f7d29dd05395e91894ec165192ca8c6a7d552a
JhomarkA <jhomark@sitesphil.com>
2025-07-16 01:49:49 +0800

    changes on the config.ts

--- amplify/config.ts
+++ amplify/config.ts
@@ -1,7 +1,7 @@
 export const config = {
-  USER_POOL_ID: 'ap-southeast-1_sfC1YPuEq',
+  USER_POOL_ID: 'ap-southeast-1_DilYeRpIg',
   REGION: 'ap-southeast-1',
-  CONTENT_TABLE: 'Content-ysjl3qu4l5hqzduhk7jxbmzxou-NONE',
-  CONTENTTOUSER_TABLE: 'contentToUser-ysjl3qu4l5hqzduhk7jxbmzxou-NONE',
-  CUSTOMFIELDS_TABLE: 'customFields-ysjl3qu4l5hqzduhk7jxbmzxou-NONE',
+  CONTENT_TABLE: 'Content-yadmibjwpbgh7k7qylqq6ae4we-NONE',
+  CONTENTTOUSER_TABLE: 'contentToUser-yadmibjwpbgh7k7qylqq6ae4we-NONE',
+  CUSTOMFIELDS_TABLE: 'customFields-yadmibjwpbgh7k7qylqq6ae4we-NONE',
 };
